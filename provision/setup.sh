571c11ac3d6410d1355bc9b50d4ecf70d92fddfd
Asier Abreu <asierabreu1@gmail.com>
2017-11-09 22:27:59 +0100

    updated

--- provision/setup.sh
+++ provision/setup.sh
@@ -4,11 +4,13 @@ echo "--------------------------------"
 echo "Provisioning virtual machine ..."
 echo "--------------------------------"
 
-SW_DIR=./software
+SW_DIR=./software/
+SPARK_PKG=spark-2.2.0-bin-hadoop2.7.tgz
+ZEPPL_PKG=zeppelin-0.7.3-bin-all.tgz
+
 mkdir $SW_DIR
 
 # Java (open-jdk-8) installation (quietly -qq and pip messages from dpkg to dev null)
-# quiet option : -qq -o Dpkg::Use-Pty=0
 echo "Installing open-jdk-8 ... please wait"
 apt-get -qq -o Dpkg::Use-Pty=0 install openjdk-8-jre-headless > /dev/null
 
@@ -17,38 +19,21 @@ echo "Installing Firefox Web Browser ... please wait"
 apt-get -qq -o Dpkg::Use-Pty=0 install firefox > /dev/null
 
 # Python 3.6
-# echo "Installing Python ... please wait"
-# apt-get -qq -o Dpkg::Use-Pty=0 install python3 > /dev/null
+echo "Installing Python ... please wait"
+apt-get -qq -o Dpkg::Use-Pty=0 install python3 > /dev/null
 
-# Miniconda Download (with timeout) and (quiet) Installation
-# echo "Installing Miniconda (Python 3.6) ... please wait"
-# wget --timeout 5 http://repo.continuum.io/miniconda/Miniconda3-3.7.0-Linux-x86_64.sh 
-# mv Miniconda3-3.7.0-Linux-x86_64.sh $SW_DIR
-# bash $SW_DIR/Miniconda3-3.7.0-Linux-x86_64.s -b -p $SW_DIR/miniconda > /dev/null
+# Uncomment below for installation : Spark 2.2.0 Download (with timeout) and (quiet) Installation if enabled 
+# echo "Installing Apache-Spark ... please wait"
+# wget --timeout 5 http://mirrors.ocf.berkeley.edu/apache/spark/spark-2.2.0/$SPARK_PKG
+# mv $SPARK_PKG $SW_DIR
+# tar zxf $SW_DIR/$SPARK_PKG -C $SW_DIR/	
 
-# Full Anaconda Download (with timeout) and (quiet) Installation 
-echo "Installing Anaconda (Python 3.6) ... please wait"
-wget --timeout 5 https://repo.continuum.io/archive/Anaconda3-5.0.1-Linux-x86_64.sh
-mv Anaconda3-5.0.1-Linux-x86_64.sh $SW_DIR
-bash $SW_DIR/Anaconda3-5.0.1-Linux-x86_64.sh -b -p $SW_DIR/anaconda > /dev/null
-
-# Spark 2.2.0 Download (with timeout) and (quiet) Installation 
-echo "Installing Apache-Spark 2.2.0 ... please wait"
-wget --timeout 5 http://mirrors.ocf.berkeley.edu/apache/spark/spark-2.2.0/spark-2.2.0-bin-hadoop2.7.tgz
-mv spark-2.2.0-bin-hadoop2.7.tgz $SW_DIR
-tar zxf $SW_DIR/spark-2.2.0-bin-hadoop2.7.tgz -C $SW_DIR/	
-
-# Zepellin Download (with timeout) and (quiet) Installation 
-echo "Installing Apache-Zepellin ... please wait"
-wget --timeout 5 http://apache.cs.utah.edu/zeppelin/zeppelin-0.7.3/zeppelin-0.7.3-bin-all.tgz
-mv zeppelin-0.7.3-bin-all.tgz $SW_DIR
-tar zxf $SW_DIR/zeppelin-0.7.3-bin-all.tgz -C $SW_DIR/	
-
-# Add SPARK home and PYTHON home to bashrc
-echo "export SPARK_HOME=$SW_DIR/spark-2.2.0-bin-hadoop2.7" >> .bashrc
-echo "export PYTHON_HOME=$SW_DIR/anaconda" >> .bashrc 
-echo "export ZEPPELIN_HOME=$SW_DIR/zeppelin-0.7.3-bin-all" >> .bashrc
-echo "export PATH=$PYTHON_HOME/bin:$PATH" >> .bashrc 
+# Uncomment below for installation : Zepellin Download (with timeout) and (quiet) Installation 
+# echo "Installing Apache-Zepellin ... please wait"
+# wget --timeout 5 http://apache.cs.utah.edu/zeppelin/zeppelin-0.7.3/$ZEPPL_PKG
+# mv $ZEPPL_PKG $SW_DIR
+# tar zxf $SW_DIR/$ZEPPL_PKG -C $SW_DIR/	
+# fi
 
 echo "--------------------------------------"
 echo "Provisioning virtual machine finished!"
