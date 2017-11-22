f8bc0e54122e7accbe85b89467440ecdee4783d2
Asier Abreu <asierabreu1@gmail.com>
2017-11-22 23:45:47 +0100

    updated

--- scripts/start_zeppelin.sh
+++ scripts/start_zeppelin.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/build:$PYTHONPATH
-export PYTHONPATH=$SPARK_HOME/python/lib/py4j-0.8.2.1-src.zip:$PYTHONPATH
 # Launch Zeppelin Daemon
 $ZEPPELIN_HOME/bin/zeppelin-daemon.sh start
 echo ""
