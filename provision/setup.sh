3a00fdd3e019f47df1910bb85a77358b3124cadb
Mercedes Ramos <mramos@gaiam01.local>
2017-11-22 01:47:36 +0100

    updated

--- provision/setup.sh
+++ provision/setup.sh
@@ -11,21 +11,6 @@ ZEPEL_DIR='/usr/local/zeppelin'
 
 echo "Provisioning virtual machine..."
 
-# Install all OS dependencies for notebook server that starts but lacks all
-# features (e.g., download as all possible file formats)
-apt-get install -yq --no-install-recommends \
-    bzip2 \
-    ca-certificates \
-    sudo \
-    locales \
-    fonts-liberation \
- && apt-get clean \
- && rm -rf /var/lib/apt/lists/*
-
-# Update LOCALE (required for proper functioning of some packages) 
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen && \
-locale-gen
-
 # Update variables Configure environment
 SHELL=/bin/bash 
 NB_USER=spark 
@@ -39,14 +24,11 @@ PATH=$CONDA_DIR/bin:$PATH
 fix-permissions /usr/local/bin/fix-permissions
 
 useradd -m -s /bin/bash -N -u $NB_UID $NB_USER && \
-    mkdir -p $CONDA_DIR && \
-    mkdir -p $SPARK_DIR && \
-    mkdir -p $ZEPEL_DIR && \
     chown $NB_USER:$NB_GID $CONDA_DIR && \
     fix-permissions $HOME && \
-fix-permissions $CONDA_DIR && \
-fix-permissions $SPARK_DIR && \
-fix-permissions $ZEPEL_DIR
+    fix-permissions $CONDA_DIR && \
+    fix-permissions $SPARK_DIR && \
+    fix-permissions $ZEPEL_DIR
 
 # Java
 echo "Installing ${JAVA_VERSION} ..."
@@ -57,25 +39,34 @@ apt-get install $JAVA_VERSION -yq --no-install-recommends \
 # Anaconda (Python with some extra interesting stuff) in silent mode , i.e, no prompts
 echo "Installing Anaconda (Python 3.6) ..."
 wget https://repo.continuum.io/archive/$CONDA_VERSION
-bash Anaconda3-5.0.1-Linux-x86_64.sh -b -p $CONDA_DIR
+bash $CONDA_VERSION -b -p $CONDA_DIR
 
 # Spark 2.2.0
 echo "Installing Spark ${SPARK_VERSION} ..."
-wget http://ftp.cixug.es/apache/spark/spark-2.2.0/$SPARK_VERSION
-tar zxf $SPARK_VERSION –C $SPARK_DIR	
+mkdir $SPARK_DIR
+wget --timeout=1 --tries=5 --retry-connrefused http://apache.uvigo.es/spark/spark-2.2.0/$SPARK_VERSION
+mv $SPARK_VERSION $SPARK_DIR
+cd $SPARK_DIR
+tar zxf $SPARK_VERSION
+cd 	
 
 # Zepellin 
 echo "Installing Zeppelin ${ZEPEL_VERSION} ..."
-wget http://ftp.cixug.es/apache/zeppelin/zeppelin-0.7.3/$ZEPEL_VERSION
-tar zxf $ZEPEL_VERSION –C $ZEPEL_DIR	
+mkdir $ZEPEL_DIR
+wget --timeout=1 --tries=5 --retry-connrefused http://apache.uvigo.es/zeppelin/zeppelin-0.7.3/$ZEPEL_VERSION
+cd $ZEPEL_DIR
+tar zxf $ZEPEL_VERSION
+cd 	
+
+# update path
+PATH=$SPARK_DIR/bin:$SPARK_DIR/sbin:$ZEPEL_DIR/bin:$PATH
 
 # copy executables to bin
-cp scripts/start_notebook.sh /usr/local/bin/
-cp scripts/start_zeppelin.sh /usr/local/bin/
-cp config/jupyter_notebook_config.py /etc/jupyter/
-RUN fix-permissions /etc/jupyter/
+cp /scripts/start_notebook.sh /usr/local/bin/
+cp /scripts/start_zeppelin.sh /usr/local/bin/
 
 echo "Finished provisioning."
-echo "You can now start ... "
-echo "(1) : PySpark (using Jupyter Notebook) by typing 'start_notebook.sh'"
-echo "(2) : Zeppelin by typing 'start_zeppelin.sh'"
+echo "(0) : type 'vagrant ssh' to login to the newly created VM "
+echo " --> Once you have logged into the VM : "
+echo "(1) : type 'start_notebook.sh' to launch PySpark (using Jupyter Notebook)"
+echo "(2) : type 'start_zepellin.sh' to launch Zeppelin server"
