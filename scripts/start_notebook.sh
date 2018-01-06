e1fc5a37f19a896677bb3de10a3f4b546b16283f
Asier Abreu <Asier Abreu@esa-ad.esa.int>
2018-01-06 01:49:17 +0100

    updated

--- scripts/start_notebook.sh
+++ scripts/start_notebook.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-PYSPARK_DRIVER_PYTHON="$CONDA_HOME/bin/jupyter" PYSPARK_DRIVER_PYTHON_OPTS="notebook --notebook-dir='$HOME/Notebooks/'" $SPARK_HOME/bin/pyspark
+PYSPARK_DRIVER_PYTHON="$CONDA_HOME/bin/jupyter" \
+PYSPARK_DRIVER_PYTHON_OPTS="notebook --port 7070 --notebook-dir='/spark-course/Notebooks/' --no-browser --ip='*'"  $SPARK_HOME/bin/pyspark \
+--master local[*] --executor-memory 2048m --driver-memory 2048m
