e349ef689e22398f9f356fac4003a56adc8cc4da
asierabreu <asier.abreu@yahoo.es>
2018-01-09 21:14:03 +0100

    udpated

--- provision/virtual_machine_setup.sh
+++ provision/virtual_machine_setup.sh
@@ -2,6 +2,7 @@
 
 JAVA_VERSION='openjdk-8-jre-headless'
 SPARK_VERSION='spark-2.2.0-bin-hadoop2.7'
+ZEPPELIN_VERSION="zeppelin-0.7.3-bin-all"
 CONDA_INST='Anaconda3-5.0.1-Linux-x86_64.sh'
 EXTENSION='.tgz'
 SPARK_TGZ=$SPARK_VERSION$EXTENSION
@@ -9,8 +10,12 @@ ZEPPELIN_TGZ=$ZEPPELIN_VERSION$EXTENSION
 
 SW_DIR="/usr/local/software"
 
-echo "Creating software directory : $SW_DIR"  
-mkdir -p $SW_DIR
+if [ d "$SW_DIR" ];then
+  echo 'directory : $SW_DIR already existing continuing' 
+else 
+  echo "Creating software directory : $SW_DIR"  
+  mkdir -p $SW_DIR
+fi
 
 SPARK_DIR="$SW_DIR/spark"
 ZEPPE_DIR="$SW_DIR/zeppelin"
@@ -31,26 +36,38 @@ update-rc.d ntp defaults
 apt-get -qq install $JAVA_VERSION
 
 # Anaconda (Python with some extra interesting stuff) in silent mode , i.e, no prompts
-echo "Installing Anaconda (Python 3.6) ..."
-bash /spark-course/provision/$CONDA_INST -b -p $CONDA_DIR
 
+if [ -d "$CONDA_DIR" ];then
+  echo 'conda installation already existing , continuing'  
+else
+  echo "Installing Anaconda (Python 3.6) ..."
+  bash /spark-course/provision/$CONDA_INST -b -p $CONDA_DIR
+fi
+  
 # Spark 2.2.0
-echo "Installing Apache Spark ${SPARK_VERSION} ..."
-cp /spark-course/provision/$SPARK_TGZ $SW_DIR
-cd $SW_DIR
-tar zxf $SPARK_TGZ
-ln -s $SPARK_VERSION spark
-mkdir "spark/logs"
-cd 
-
+if [ -d "$SPARK_DIR" ];then
+  echo 'spark installation already existing , continuing'  
+else
+  echo "Installing Apache Spark ${SPARK_VERSION} ..."
+  cp /spark-course/provision/$SPARK_TGZ $SW_DIR
+  cd $SW_DIR
+  tar zxf $SPARK_TGZ
+  ln -s $SPARK_VERSION spark
+  mkdir "spark/logs"
+  cd 
+fi
 # Zeppelin 0.7.3
-echo "Installing Apache Zeppelin ${ZEPPELIN_VERSION} ..."
-cp /spark-course/provision/$ZEPPELIN_TGZ $SW_DIR
-cd $SW_DIR
-tar zxf $ZEPPELIN_TGZ
-ln -s $ZEPPELIN_VERSION zeppelin
-mkdir "zeppelin/logs"
-cd 
+#if [ -d "$ZEPPE_DIR" ];then
+#  echo 'zeppelin installation already existing , continuing'  
+#else
+#  echo "Installing Apache Zeppelin ${ZEPPELIN_VERSION} ..."
+#  cp /spark-course/provision/$ZEPPELIN_TGZ $SW_DIR
+#  cd $SW_DIR
+#  tar zxf $ZEPPELIN_TGZ
+#  ln -s $ZEPPELIN_VERSION zeppelin
+#  mkdir "zeppelin/logs"
+#  cd 
+#fi
 
 # Update ownership of directories and contents
 chown ubuntu:ubuntu -R $SW_DIR
