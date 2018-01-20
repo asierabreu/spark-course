1f86d3958d7341e935d81937fc2e434a1754bb07
Ubuntu <ubuntu@ip-172-31-33-105.eu-central-1.compute.internal>
2018-01-20 10:37:26 +0000

    added

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -1,4 +1,4 @@
-$set_env_vars=<<SCRIPT
+#!/bin/bash
 tee "/etc/profile.d/myvars.sh" > "/dev/null"<<EOF
 export JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64/jre/"
 export SW_DIR="/usr/local/software"
@@ -6,4 +6,4 @@ export SPARK_HOME="/usr/local/software/spark"
 export ZEPPELIN_HOME="/usr/local/software/zeppelin"
 export CONDA_HOME="/usr/local/software/conda"
 export PATH="/usr/local/software/conda/bin:/usr/local/software/spark/bin:/usr/local/software/spark/sbin:/usr/local/software/zeppelin/bin:$PATH"
-SCRIPT
+EOF
