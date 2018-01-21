1d041256546be4e36f2427acdc4a9ad15d967483
Ubuntu <ubuntu@ip-172-31-32-145.eu-central-1.compute.internal>
2018-01-21 13:26:45 +0000

    updated

---
+++ scripts/ambari_agent_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+wget -O /etc/apt/sources.list.d/ambari.list http://public-repo-1.hortonworks.com/ambari/ubuntu16/2.x/updates/2.6.0.0/ambari.list
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com B9733A7A07513CAD
+apt-get update
+
+echo 'Installing Apache Ambari agent ...' 
+apt-get -qq install ambari-agent
+
+echo 'Setting up Apache Ambari agent ...'
+machine_ip=$(cat /etc/hostname)
+tee "/etc/ambari-agent/conf/ambari-agent.ini" > "/dev/null"<<EOF
+[server]
+hostname=$(cat /etc/hostname)
+url_port=8440
+secured_url_port=8441
+piddir=/var/run/ambari-agent/
+logdir=/var/log/ambari-agent/
+keysdir=/root/.ssh/
+EOF
+echo 'Done.'
