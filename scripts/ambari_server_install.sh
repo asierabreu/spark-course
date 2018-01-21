f45a9dbc0617a8901b19fc2273ee30edd752bbec
Ubuntu <ubuntu@ip-172-31-32-145.eu-central-1.compute.internal>
2018-01-21 13:41:56 +0000

    updated

---
+++ scripts/ambari_server_install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+wget -O /etc/apt/sources.list.d/ambari.list http://public-repo-1.hortonworks.com/ambari/ubuntu16/2.x/updates/2.6.0.0/ambari.list
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com B9733A7A07513CAD
+apt-get update
+
+# Install Ambari
+echo 'Installing Apache Ambari server ...' 
+apt-get -qq install ambari-server
+
+echo 'Setting up Apache Ambari server ...'
+ambari-server setup -s
