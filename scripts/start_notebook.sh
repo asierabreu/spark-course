e11eebaf68b7739cdda9b723f5aeba1e0acd862a
Ubuntu <ubuntu@master.mycluster>
2018-01-30 17:32:59 +0000

    updated

--- scripts/start_notebook.sh
+++ scripts/start_notebook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 PYSPARK_DRIVER_PYTHON="$CONDA_HOME/bin/jupyter" \
-PYSPARK_DRIVER_PYTHON_OPTS="notebook --port 7070 --no-browser --ip='*'"  $SPARK_HOME/bin/pyspark \
+PYSPARK_DRIVER_PYTHON_OPTS="notebook --port 7070 --notebook-dir='$HOME/spark-course/Notebooks/' --no-browser --ip='*'"  $SPARK_HOME/bin/pyspark \
 --master local[*] --executor-memory 2048m --driver-memory 2048m
