a0c826666dea2e687584858263ce16258e002b20
Asier Abreu Arramburu <asier.abreu@altran.es>
2018-11-04 16:51:33 +0100

    updated

--- provision/vm_setup.sh
+++ provision/vm_setup.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
 
-JAVA_VERSION='openjdk-8-jre-headless'
-SPARK_VERSION='spark-2.2.0-bin-hadoop2.7'
-ZEPPELIN_VERSION="zeppelin-0.7.3-bin-all"
-CONDA_INST='Anaconda3-5.0.1-Linux-x86_64.sh'
-EXTENSION='.tgz'
-SPARK_TGZ=$SPARK_VERSION$EXTENSION
-ZEPPELIN_TGZ=$ZEPPELIN_VERSION$EXTENSION
-
-SW_DIR="/usr/local/software"
-
 PROVISION_DIR=$1
 
 if [ -z $PROVISION_DIR];then
   echo 'Must provide a path to the downloaded software location! (./vm_setup.sh <software_packages_loaction>)'
   exit 
 fi  
+
+# Get the downloaded versions 
+JAVA_VERSION='openjdk-8-jre-headless' # fixed
+
+SPARK_TGZ=$(basename $(ls -1 $PROVISION_DIR/spark-*))
+ZEPPE_TGZ=$(basename $(ls -1 $PROVISION_DIR/zeppelin-*))
+CONDA_TSH=$(basename $(ls -1 $PROVISION_DIR/Miniconda-*))
+
+SPARK_VERSION="${SPARK_TGZ%.*}"
+ZEPPE_VERSION="${ZEPPE_TGZ%.*}"
+CONDA_VERSION="${CONDA_TSH%.*}"
+
+echo 'Spark Version : ' $SPARK_VERSION
+echo 'Zeppelin Version : ' $ZEPPE_VERSION
+echo 'Conda Version : ' $CONDA_VERSION
+
+SW_DIR="/usr/local/software/"
 
 if [ -d "$SW_DIR" ];then
   echo 'directory : $SW_DIR already existing continuing' 
@@ -26,7 +33,7 @@ fi
 
 SPARK_DIR="$SW_DIR/spark"
 ZEPPE_DIR="$SW_DIR/zeppelin"
-CONDA_DIR="$SW_DIR/conda"
+CONDA_DIR="$SW_DIR/miniconda"
 
 if [ ! -d "$SW_DIR" ];then
   echo 'could not create directory : $SW_DIR aborting installation' 
@@ -47,15 +54,15 @@ apt-get -qq install $JAVA_VERSION
 if [ -d "$CONDA_DIR" ];then
   echo 'conda installation already existing , continuing'  
 else
-  echo "Installing Anaconda (Python 3.6) ..."
-  bash $PROVISION_DIR/$CONDA_INST -b -p $CONDA_DIR
+  echo "Installing Anaconda ..."
+  bash $PROVISION_DIR/$CONDA_TSH -b -p $CONDA_DIR
 fi
   
 # Spark 
 if [ -d "$SPARK_DIR" ];then
   echo 'spark installation already existing , continuing'  
 else
-  echo "Installing Apache Spark ${SPARK_VERSION} ..."
+  echo "Installing Apache Spark ..."
   cp $PROVISION_DIR/$SPARK_TGZ $SW_DIR
   cd $SW_DIR
   tar zxf $SPARK_TGZ
@@ -67,10 +74,10 @@ fi
 if [ -d "$ZEPPE_DIR" ];then
   echo 'zeppelin installation already existing , continuing'  
 else
-  echo "Installing Apache Zeppelin ${ZEPPELIN_VERSION} ..."
-  cp $PROVISION_DIR/$ZEPPELIN_TGZ $SW_DIR
+  echo "Installing Apache Zeppelin ..."
+  cp $PROVISION_DIR/$ZEPPE_TGZ $SW_DIR
   cd $SW_DIR
-  tar zxf $ZEPPELIN_TGZ
+  tar zxf $ZEPPE_TGZ
   ln -s $ZEPPELIN_VERSION zeppelin
   mkdir "zeppelin/logs"
   cd 
