e099bf9043a81c473c1680f5024e2f30a1267369
Asier Abreu Arramburu <asier.abreu@altran.es>
2018-11-04 16:42:58 +0100

    updated

--- scripts/download_sw.sh
+++ scripts/download_sw.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-JAVA_VERSION='openjdk-8-jre-headless'
-SPARK_VERSION='spark-2.2.0-bin-hadoop2.7'
-ZEPPELIN_VERSION='zeppelin-0.7.3-bin-all'
-CONDA_INST='Anaconda3-5.0.1-Linux-x86_64.sh'
-EXTENSION='.tgz'
-SPARK_TGZ=$SPARK_VERSION$EXTENSION
-ZEPPELIN_TGZ=$ZEPPELIN_VERSION$EXTENSION
 
 PROVISION_DIR="provision"
 if [ ! -d "$PROVISION_DIR" ];then
   echo 'please download first the git repo : git clone https://github.com/asierabreu/spark-course' 
   exit
 fi
+
+# specify version to be downloaded
+MINICONDA='Miniconda3-latest-Linux-x86_64.sh'
+SPARK='spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz'
+ZEPPELIN='zeppelin-0.8.0/zeppelin-0.8.0-bin-all.tgz'
 cd $PROVISION_DIR
-echo 'Downloading Python Anaconda3-5 ...'
-curl -O -# https://repo.continuum.io/archive/$CONDA_INST
-echo 'Downloading Apache Spark 2.2.0 ...'
-curl -O -# http://mirror.cogentco.com/pub/apache/spark/spark-2.2.0/$SPARK_TGZ
-#echo 'Downloading Apache Zeppellin 0.7.3 ...'
-#curl -O -# http://apache.uvigo.es/zeppelin/zeppelin-0.7.3/$ZEPPELIN_TGZ
+echo 'Downloading Anaconda ...'
+curl -O -# https://repo.continuum.io/miniconda/$MINICONDA
+echo 'Downloading Apache Spark ...'
+curl -O -# http://apache.rediris.es/spark/$SPARK
+echo 'Downloading Apache Zeppellin ...'
+curl -O -# http://apache.rediris.es/zeppelin/$ZEPPELIN
 cd 
