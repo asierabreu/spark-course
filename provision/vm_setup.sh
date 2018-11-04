3375830243702d5690db5c26c4c3ee97a3232e69
Asier Abreu Arramburu <asier.abreu@altran.es>
2018-11-04 17:22:19 +0100

    updated

--- provision/vm_setup.sh
+++ provision/vm_setup.sh
@@ -95,6 +95,6 @@ echo "Updating environment ..."
 # Copy some executables to the path
 cp $HOME/spark-course/scripts/* /usr/local/bin/
 # Update the bashrc to call the environment variables setup
-cp '$HOME/spark-course/scripts/bash_profile' $HOME/.bash_profile
+cp 'scripts/bash_profile' $HOME/.bash_profile
 
 echo 'Provisioning finished'
