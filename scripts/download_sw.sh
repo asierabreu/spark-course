0dc4a7b671bd58704a098c1741492d0601c61841
Asier Abreu Arramburu <asier.abreu@altran.es>
2018-11-04 21:03:10 +0100

    updated

--- scripts/download_sw.sh
+++ scripts/download_sw.sh
@@ -7,12 +7,14 @@ if [ ! -d "$PROVISION_DIR" ];then
 fi
 
 # specify version to be downloaded
+ANACONDA='Anaconda3-latest-Linux-x86_64.sh'
 MINICONDA='Miniconda3-latest-Linux-x86_64.sh'
 SPARK='spark-2.3.2/spark-2.3.2-bin-hadoop2.7.tgz'
 ZEPPELIN='zeppelin-0.8.0/zeppelin-0.8.0-bin-all.tgz'
 cd $PROVISION_DIR
 echo 'Downloading Anaconda ...'
-curl -O -# https://repo.continuum.io/miniconda/$MINICONDA
+# curl -O -# https://repo.continuum.io/miniconda/$MINICONDA
+curl -O -# https://repo.continuum.io/archive/$ANACONDA
 echo 'Downloading Apache Spark ...'
 curl -O -# http://apache.rediris.es/spark/$SPARK
 echo 'Downloading Apache Zeppellin ...'
